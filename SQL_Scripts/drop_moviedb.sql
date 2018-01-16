c8abea3f93abb5f13ccd798dffb68bbd1b7ac000
zwalls <zwalls@uci.edu>
2018-01-15 23:46:00 -0800

    Added sql scripts

---
+++ SQL_Scripts/drop_moviedb.sql
@@ -0,0 +1,12 @@
+USE `moviedb`;
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE ratings;
+DROP TABLE creditcards;
+DROP TABLE customers;
+DROP TABLE sales;
+DROP TABLE genres_in_movies;
+DROP TABLE genres;
+DROP TABLE stars_in_movies;
+DROP TABLE stars;
+DROP TABLE movies;
