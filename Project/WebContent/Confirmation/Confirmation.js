ad476d57d2a4af9b870c0464784239c049e188a4
zwalls <zwalls@uci.edu>
2018-02-09 23:16:42 -0800

    changed confirmation page

--- Project/WebContent/Confirmation/Confirmation.js
+++ Project/WebContent/Confirmation/Confirmation.js
@@ -1,8 +1,30 @@
 
 (function(){
-    localStorage.setItem('cart', JSON.stringify(emptyCart));
+    var isValid = getParameterByName("isValid");
+    var header = jQuery("#Message");
+
+    if(isValid !== "true"){
+        header.append("<h1>Purchase cannot complete, Please enter a valid card.</h1>");
+    }
+    else {
+        header.append("<h1>Thank you for your purchase!</h1>");
+        localStorage.setItem('cart', JSON.stringify(emptyCart));
+    }
+
+
 })();
 
 function GoHome() {
     window.location.assign("Main");
+}
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
--- Project/WebContent/CustomerInfo/CustomerInfo.js
+++ Project/WebContent/CustomerInfo/CustomerInfo.js
@@ -15,5 +15,34 @@ function sendData(){
 })();
 
 function sendData(){
-    window.location.assign("Confirmation");
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var cardNumber = document.getElementById("cardNumber").value;
+    var cardExpiration = document.getElementById("cardExpiration").value;
+    var address = document.getElementById("address").value;
+    var cart = JSON.parse(localStorage.getItem("cart")) || [];
+    jQuery.ajax({
+        dataType: "json",
+        method: "Get",
+        url: "/Project/api/movie",
+        data: {
+            ACTION: "cred",
+            FirstName: firstName,
+            LastName: lastName,
+            Address: address,
+            CardNumber: cardNumber,
+            CardExpiration: cardExpiration,
+            Cart: JSON.stringify(cart)
+        },
+        success: function(resultData){
+            if(resultData[0] === "yes")
+                window.location.assign("Confirmation?isValid=true");
+            else
+                window.location.assign("Confirmation?isValid=false");
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+            alert(textStatus);
+        }
+    });
+
 }
