0c6eee02437d820ffe2afb6bd0889df444ff9349
zwalls <zwalls@uci.edu>
2018-02-20 16:16:22 -0800

    stored procedure stuff

--- SQL_Scripts/add_movie.sql
+++ SQL_Scripts/add_movie.sql
@@ -1,7 +1,7 @@
 USE `moviedb`;
 Delimiter $$
 CREATE PROCEDURE add_movie (IN id VARCHAR(10), IN title VARCHAR(100), IN year INT, IN director VARCHAR(100), 
-							IN star VARCHAR(100), IN starName VARCHAR(100), IN starBirthYear INT, 
+							IN sId VARCHAR(10), IN star VARCHAR(100), IN starName VARCHAR(100), IN starBirthYear INT, 
 							IN genre VARCHAR(32))
 Begin
 	DECLARE checkStar int;
@@ -17,7 +17,7 @@ Begin
     VALUES (@id, @title, @year, @director);
     SELECT id INTO movieId FROM movies WHERE title = @title;
     
-    SELECT count(*) INTO checkStar from stars WHERE name = @star; 
+    SELECT count(*) INTO checkStar from stars WHERE id = @sId; 
     SELECT count(*) INTO checkGenre from genres WHERE name = @genre; 
     
     IF (checkStar > 0) THEN
@@ -25,11 +25,10 @@ Begin
         INSERT INTO stars_in_movies (starId, movieId)
         VALUES (starId, movieId);
 	ELSE
-		INSERT INTO stars (name, birthYear)
-        VALUES (@starName, @starBirthYear);
-        SELECT id INTO starId FROM stars WHERE name = @star;
+		INSERT INTO stars (id, name, birthYear)
+        VALUES (@sId, @starName, @starBirthYear);
         INSERT INTO stars_in_movies (starId, movieId)
-        VALUES (starId, @id);
+        VALUES (@sId, @id);
 	END IF;
     
     IF (checkGenre > 0) THEN
